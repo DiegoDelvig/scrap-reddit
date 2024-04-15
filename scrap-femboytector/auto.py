ea06836bb5564a3ce99a8beaf628b428f699988b
Diego Delvig <157409019+DiegoDelvig@users.noreply.github.com>
2024-04-15 13:25:35 +0200

    Add files via upload

---
+++ scrap-femboytector/auto.py
@@ -0,0 +1,6 @@
+import os, time
+
+while True:
+	os.system("/bin/bash -c \"" + "python3 main.py femboy" + "\"") 
+	os.system("/bin/bash -c \"" + "python3 main.py GothGirls" + "\"") 
+
---
+++ scrap-femboytector/database.py
@@ -0,0 +1,11 @@
+import firebase_admin
+from firebase_admin import db, credentials
+
+def add(element, dir):
+	ref = db.reference(f'/{dir}')
+	ref.set(element)
+
+
+def connect():
+	cred = credentials.Certificate("credentials.json")
+	firebase_admin.initialize_app(cred, {'databaseURL': "https://femboytector-default-rtdb.europe-west1.firebasedatabase.app"})
